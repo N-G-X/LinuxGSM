2992022e74e7275abde7e29f0e310b74cff449b1
N-G-X <jeeprukus@gmail.com>
2018-01-20 17:00:34 -0700

    Updating for arkserver

--- lgsm/functions/alert_discord.sh
+++ lgsm/functions/alert_discord.sh
@@ -7,46 +7,46 @@
 
 json=$(cat <<EOF
 {
-"username":"LinuxGSM",
-"avatar_url":"https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/images/logo/lgsm-dark-square-512.png",
+"username":"Server Ninja",
+"avatar_url":"https://raw.githubusercontent.com/N-G-X/LinuxGSM/master/images/logo/NinjaheadFinal.png",
 "file":"content",
 
 "embeds": [{
-	"color": "2067276",
-	"author": {"name": "${alertemoji} ${alertsubject} ${alertemoji}", "icon_url": "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/images/logo/lgsm-dark-square-512.png"},
-	"title": "",
-	"description": "",
-	"url": "",
-	"type": "content",
-	"thumbnail": {"url": "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/images/logo/lgsm-dark-square-512.png"},
-	"footer": {"text": "LinuxGSM", "icon_url": "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/images/logo/lgsm-dark-square-512.png"},
-	"fields": [
-			{
-				"name": "Alert Message",
-				"value": "${alertbody}"
-			},
-			{
-				"name": "Game",
-				"value": "${gamename}"
-			},
-			{
-				"name": "Server name",
-				"value": "${servername}"
-			},
-			{
-				"name": "Hostname",
-				"value": "${HOSTNAME}"
-			},
-			{
-				"name": "Server IP",
-				"value": "[${ip}:${port}](https://www.gametracker.com/server_info/${ip}:${port})"
-			},
-			{
-				"name": "More info",
-				"value": "${alerturl}"
-			}
-		]
-	}]
+        "color": "2067276",
+        "author": {"name": "${alertemoji} ${alertsubject} ${alertemoji}", "icon_url": "https://raw.githubusercontent.com/N-G-X/LinuxGSM/master/images/logo/NinjaheadFinal.png"},
+        "title": "",
+        "description": "",
+        "url": "",
+        "type": "content",
+        "thumbnail": {"url": "https://raw.githubusercontent.com/N-G-X/LinuxGSM/master/images/logo/NinjaheadFinal.png"},
+        "footer": {"text": "Server Ninja", "icon_url": "https://raw.githubusercontent.com/N-G-X/LinuxGSM/master/images/logo/NinjaheadFinal.png"},
+        "fields": [
+                        {
+                                "name": "Alert Message",
+                                "value": "${alertbody}"
+                        },
+                        {
+                                "name": "Game",
+                                "value": "${gamename}"
+                        },
+                        {
+                                "name": "Server name",
+                                "value": "${servername}"
+                        },
+                        {
+                                "name": "Hostname",
+                                "value": "${HOSTNAME}"
+                        },
+                        {
+                                "name": "Server IP",
+                                "value": "[64.183.143.74:27017](https://www.gametracker.com/server_info/64.183.143.74:27017)"
+                        },
+                        {
+                                "name": "More info",
+                                "value": "${alerturl}"
+                        }
+                ]
+        }]
 }
 EOF
 )
@@ -56,9 +56,9 @@ sleep 0.5
 discordsend=$(${curlpath} -sSL -H "Content-Type: application/json" -X POST -d """${json}""" ${discordwebhook})
 
 if [ -n "${discordsend}" ]; then
-	fn_print_fail_nl "Sending Discord alert: ${discordsend}"
-	fn_script_log_fatal "Sending Discord alert: ${discordsend}"
+        fn_print_fail_nl "Sending Discord alert: ${discordsend}"
+        fn_script_log_fatal "Sending Discord alert: ${discordsend}"
 else
-	fn_print_ok_nl "Sending Discord alert"
-	fn_script_log_pass "Sending Discord alert"
+        fn_print_ok_nl "Sending Discord alert"
+        fn_script_log_pass "Sending Discord alert"
 fi
